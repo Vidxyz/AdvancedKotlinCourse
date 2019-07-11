649a31084aefb7000d6e89721716d5d7c18f6ed3
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-07-11 15:15:39 -0400

    Contravariance (in variance) example

--- src/com/vid/kotlincourse/generics/Covariance.kt
+++ src/com/vid/kotlincourse/generics/Covariance.kt
@@ -15,12 +15,18 @@ fun operator(person: List<Person>) {
 
 
 // Covariance is now respected
+//values are passed OUT (not in)
 interface RORepo<out T> {
     fun getId(id: Int): T
     fun getAll(): List<T>
 }
 
-
+// Declaration site variance
+// Contravariance, opposite of covariance (not the intuitive scenario this)
+interface WriteRepot<in T> {
+    fun save(obj:T )
+    fun saveAll(list: List<T>)
+}
 
 
 fun main() {
